70777876fffed2249744d62805f40092b15450ef
hiro <jinguang.tong@anu.edu.au>
2023-12-06 17:04:15 +1100

    Revert "Revert "update""
    This reverts commit ebe3a6a1bf02d5bb16b40e9d553b44da5d43d30d.

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Dec  6 16:32:42 2023
-__version__ = '1.2.0+8758632'
+# TIME: Wed Dec  6 16:43:28 2023
+__version__ = '1.2.0+2ae3995'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
